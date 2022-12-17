16a0829a57e436ef755f5775685a03359ed81de6
PrometheusAnith <120585581+PrometheusAnith@users.noreply.github.com>
2022-12-17 16:54:58 +0530

    Add files via upload

---
+++ ASSIGNMENT 1 EDYODA.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1 Write a Python program to get the Fibonacci series between 0 to 50
+
+# In[20]:
+
+
+x=int(input('enter the number:'))
+a=0
+b=1
+sum=0
+if x<=0:
+    print("Enter a positive number:")
+else:
+    for i in range(0,51):
+        print(sum,end=" ")
+        a=b
+        b=sum
+        sum=a+b
+x*2
+ 
+
+        
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
